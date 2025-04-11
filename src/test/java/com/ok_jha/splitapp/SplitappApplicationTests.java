a0ead897bbab3114f71bb7c9b7f5fe4a88fa97f2
Om Jha <omkashyap1994@gmail.com>
2025-04-11 15:09:50 +0530

    Initial commit: Spring Boot project structure from Initializr

---
+++ src/test/java/com/ok_jha/splitapp/SplitappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ok_jha.splitapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
